e8fe2aa2f205aad198144fffa33f809929108d84
@slovacus <slovacus@gmail.com>
2021-01-21 12:03:10 -0500

    sample: implement sample for implement workspaces (#7)

---
+++ example/main.tf
@@ -0,0 +1,18 @@
+locals {
+  tags = {
+    terraform_id = var.namespace
+    Namespace    = var.namespace
+    Environment  = var.stage
+    Stage        = var.stage
+  }
+}
+
+module "main" {
+  source = "git://github.com/hadenlabs/terraform-aws-workspaces.git?ref=0.0.0"
+  providers = {
+    aws = aws.main
+  }
+
+  directory_pass = "implement password with sops"
+  tags           = local.tags
+}
---
+++ example/variables.tf
@@ -0,0 +1,9 @@
+variable "namespace" {
+  type        = string
+  description = "The namespace for project"
+}
+
+variable "stage" {
+  type        = string
+  description = "The stage for project"
+}
